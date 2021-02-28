ce68d9476ee753eb9450905a22bf56efad63179f
System Administrator <root@Yalo-120.local>
2021-02-28 03:29:58 -0600

    DesdeLocal

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: process.env.DATABASE_HOST || '127.0.0.1',
-        user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || 'root',
+        host: process.env.DATABASE_HOST || '54.188.236.107',
+        user: process.env.DATABASE_USER || 'nmdevel',
+        password: process.env.DATABASE_PASSWORD || 'nmdevel',
         database: process.env.DATABASE_NAME || 'db_links'
     },
     port: process.env.PORT || 4000
