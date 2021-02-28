3c5d20bd2f2ea94768fd41bacf43e196627c27f7
evesmax <evesmax@gmail.com>
2021-02-28 00:34:42 -0600

    Add files via upload

---
+++ churchadminsystem/src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    database: {
+        connectionLimit: 10,
+        host: process.env.DATABASE_HOST || '127.0.0.1',
+        user: process.env.DATABASE_USER || 'root',
+        password: process.env.DATABASE_PASSWORD || 'root',
+        database: process.env.DATABASE_NAME || 'db_links'
+    },
+    port: process.env.PORT || 4000
+};
